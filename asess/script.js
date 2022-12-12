a6f7ac4e6f10f2b0b9c7257e16088aaaef7dbbf0
binhan1610 <dangbinhan1610@gmail.com>
2022-12-12 22:51:50 +0700

    50%

--- asess/script.js
+++ asess/script.js
@@ -1,3 +1,4 @@
+//slide1
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +26,40 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+//slide2
+let slideindex2=1;
+showSlides2(slideindex2);
+function plusSlides2(n){
+  showSlides2(slideindex2+=n)
+}
+function showSlides2(n)
+{
+  let i;
+  let slides2=document.getElementsByClassName("mySlides2");
+  if(n>slides2.length) {slideindex2=1}
+  if(n<1){slideindex2=slides2.length}
+  for(i=0;i<slides2.length;i++)
+  {
+    slides2[i].style.display="none";
+  }
+  slides2[slideindex2-1].style.display="inline-block"
+}
+//slide3
+let an=1;
+show(an);
+function plus(n)
+{
+show(an+=n)
+}
+function show(n)
+{
+  let binh=document.getElementsByClassName("mySlides3")
+  if(n>binh.length) {an=1}
+  if(n<1) {an=binh.length}
+  for(let i=0;i<binh.length;i++)
+  {
+    binh[i].style.display="none"
+  }
+  binh[an-1].style.display="inline-block"
 }
