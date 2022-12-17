3fa0f84a8c6ab2657b4bee227818e479bf180a4b
binhan1610 <dangbinhan1610@gmail.com>
2022-12-17 16:28:00 +0700

    90%

--- asess/script.js
+++ asess/script.js
@@ -85,24 +85,57 @@ for(let i=0;i<color.length;i++)
 
   
 }
-for(let i=0;i<color.length;i++)
+const add=document.querySelectorAll('.them')
+const name1=document.querySelectorAll('.name')
+const money=document.querySelectorAll('.money')
+const sanpham=document.querySelector('.sanpham')
+const giatien=document.querySelector('.giatien')
+function them(i)
 {
-  let j=i
-  color[i].addEventListener('click',()=>{
-    color[i].style.border="5px solid black"
-       color[i].addEventListener('click',()=>{
-        if(color[i].style.border==="5px solid black")
-        {
-          color[i].style.border="1px solid black"
-          i=0;
-        }
-        else{
-          i++
-        }
-       })
-    
-  })
 
+  const div1=document.createElement('div')
+  
+  div1.innerHTML=name1[i].innerHTML
+
+  sanpham.appendChild(div1)
+
+}
+function themgia(i){
+  const div2=document.createElement('div')
+  div2.innerHTML=money[i].innerHTML
+  giatien.appendChild(div2)
+
+}
+for(let i=0;i<add.length;i++)
+{
+  add[i].addEventListener('click',()=>{
+    them(i)
+    themgia(i)
+    alert('đã thêm vào giỏ hàng')
+  
+  });
   
 }
+const luu=document.querySelector('.luu')
+function luudata()
+{
+  const data = [];
+        // const value = {
+        //     "congv":"congviec1"
+        // }
+        // data.push(value)
+        for(var i =1;i<sanpham.children.length;i++){
+            const value = {
+                "sanpham":sanpham.children[i].innerHTML,
+                "giatien":giatien.children[i].innerHTML
+            }
+            data.push(value)
+        }
+        console.log(data);
+        localStorage.setItem("sanpham", JSON.stringify(data));
 
+}
+luu.addEventListener('click',()=>{
+  luudata()
+  alert('ĐÃ LƯU VÀO GIỎ HÀNG')
+})
